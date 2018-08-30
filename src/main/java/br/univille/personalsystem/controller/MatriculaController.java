a1574c3cea11fae87d984997316fedb0d244bf90
CARLOS EDUARDO HOECKELE <carlos.hoeckele@univille.br>
2018-08-30 20:16:09 -0300

    trabalho

---
+++ src/main/java/br/univille/personalsystem/controller/MatriculaController.java
@@ -0,0 +1,57 @@
+package br.univille.personalsystem.controller;
+
+import java.util.List;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import br.univille.personalsystem.model.Matricula;
+import br.univille.personalsystem.repository.MatriculaRepository;
+
+@Controller
+@RequestMapping("/matricula")
+public class MatriculaController {
+	@Autowired
+    private MatriculaRepository matriculaRepository;
+
+	@GetMapping("")
+	public ModelAndView index() {
+		List<Matricula> listaMatricula = this.matriculaRepository.findAll();
+       
+        return new ModelAndView("matricula/index","listapac",listaMatricula);
+		
+	}
+    @GetMapping("/novo")
+    public String createForm(@ModelAttribute Matricula matricula) {
+    	return "matricula/form";
+    }
+    @PostMapping(params="form")
+    public ModelAndView save(@Valid Matricula matricula, BindingResult result, RedirectAttributes redirect) {        
+    	matricula = this.matriculaRepository.save(matricula);
+        return new ModelAndView("redirect:/matricula");
+    }
+    @GetMapping(value="/alterar/{id}")
+    public ModelAndView alterarForm(@PathVariable("id") Matricula matricula) {
+        return new ModelAndView("matricula/form","matricula",matricula);
+    }
+    @GetMapping(value="remover/{id}")
+    public ModelAndView remover(@PathVariable ("id") Long id, RedirectAttributes redirect) {
+        this.matriculaRepository.deleteById(id);
+        return new ModelAndView("redirect:/matricula");
+    }
+}
+
+
+
+
+
